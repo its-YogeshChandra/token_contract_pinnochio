30c77e4212efc248f8b863f67cb1337cc40c3b9b
its-YogeshChandra <pandittheroyal@gmail.com>
2026-02-05 07:44:05 +0530

    first commit

---
+++ target/release/deps/ptokenmint.d
@@ -0,0 +1,5 @@
+/Users/test/Documents/web3/pinnochio/ptokenmint/target/release/deps/ptokenmint.d: src/lib.rs
+
+/Users/test/Documents/web3/pinnochio/ptokenmint/target/release/deps/libptokenmint.dylib: src/lib.rs
+
+src/lib.rs:
---
+++ target/release/libptokenmint.d
@@ -0,0 +1,1 @@
+/Users/test/Documents/web3/pinnochio/ptokenmint/target/release/libptokenmint.dylib: /Users/test/Documents/web3/pinnochio/ptokenmint/src/lib.rs
